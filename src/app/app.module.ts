cf055bee4db198558a55a8695860474929c36087
tylerreed00 <tyler.reed00@gmail.com>
2020-10-28 20:19:22 -0500

    Homework 3, Added Navigation to the Travel App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
+//Tyler Reed
+//CSC436 Online
+//HW3
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { TicketComponent } from './ticket/ticket.component';
+import { AboutComponent } from  './about/about.component';
 import { DupeCheckDirective } from './directives/dupe-check.directive';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'ticket', component:TicketComponent},
+  {path: 'about', component:AboutComponent}
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +25,8 @@ import { DupeCheckDirective } from './directives/dupe-check.directive';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
